8009b25e2cca24ec240ff2dc32a52bdc2e4f3c46
gordonfreeman <gordonfreeman@gitlab.com>
2017-12-25 14:20:29 +0530

    Mod client code and added mqtt javascript handler

--- webserver/Sdiot_server/js/colorpicker.js
+++ webserver/Sdiot_server/js/colorpicker.js
@@ -1,7 +1,83 @@
-function sendRGB(rgb){
-    //add mqtt send logic
+// Create a client instance
+var clientid = "testclient"+Math.floor(Math.random() * 20)+Math.floor(Math.random() * 20)+Math.floor(Math.random() * 20);
+client = new Paho.MQTT.Client("13.126.106.71", Number("9001"),clientid);
+
+
+
+// set callback handlers
+
+client.onConnectionLost = onConnectionLost;
+
+client.onMessageArrived = onMessageArrived;
+
+
+
+// connect the client
+
+
+client.connect(
+{
+				cleanSession : false, 
+                onSuccess : onConnect, 
+                onFailure : onConnectionLost, 
+                keepAliveInterval: 30, 
+				}
+
+);
+
+
+
+
+// called when the client connects
+
+function onConnect() {
+
+  // Once a connection has been made, make a subscription and send a message.
+
+  console.log("onConnect");
+
+  //client.subscribe("World");
+
+  message = new Paho.MQTT.Message("Hello");
+
+  message.destinationName = "test";
+
+  client.send(message);
+
 }
 
+function sendRGB(rgb){
+	rgb = "" + rgb;
+	 var message = new Paho.MQTT.Message(rgb);
+	 message.destinationName = "cafeteria/trees";
+	 client.send(message);
+	 console.log(message);
+    console.log(rgb);
+}
+
+
+// called when the client loses its connection
+
+function onConnectionLost(responseObject) {
+
+
+  if (responseObject.errorCode !== 0) {
+
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+
+  }
+
+this.connect(
+{
+				cleanSession : false, 
+                onSuccess : onConnect, 
+                onFailure : onConnectionLost, 
+                keepAliveInterval: 30, 
+				}
+
+);
+
+}
 function sendColorWipe() {
  //var mess = "color-wipe";
  sendRGB("color-wipe");
@@ -19,12 +95,28 @@ function sendTheaterChase() {
  sendRGB("theater-chase");
 }
 
+
+
+// called when a message arrives
+
+function onMessageArrived(message) {
+
+  console.log("onMessageArrived:"+message.payloadString);
+
+}
+
 jQuery(document).ready(function($) {
+
+	/* Some simple jQuery to switch the classes */
 	$('.colourOption').on('click', function() {
-		
+		/* This line removes the selectedColour class from every element in the colouPicker, meaning that only one is selected at a time!
+
+		 It basically saves me having to make them a check box, I should proabbly be told of for that...*/
 		$('#colourPicker *').removeClass('selectedColour');
+		/* This switches on the selectedColour class for that div. */
 		$(this).toggleClass('selectedColour');
-        
+
+    /*Changing the body colours, its really ugly repeative code, I could probably improve it! */
 		if ($(this).attr('id') == 'colourOne') {
 			$('polygon').addClass('bodyOne');
 			$('polygon').removeClass('bodyTwo bodyThree bodyFour bodyFive bodySix');
